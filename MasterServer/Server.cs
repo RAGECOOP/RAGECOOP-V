a3a00c82da2fefa5d251cd9a16d9bab3c57b0bd2
EntenKoeniq <81123713+EntenKoeniq@users.noreply.github.com>
2021-08-26 17:02:54 +0200

    LAST UPDATE

--- MasterServer/Server.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MasterServer
-{
-    struct Server
-    {
-        public string Address { get; set; }
-        public int Players { get; set; }
-        public int MaxPlayers { get; set; }
-    }
-}
