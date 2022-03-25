cfca7ad41248f4e7619c217491b9670942afce57
EntenKoeniq <81123713+EntenKoeniq@users.noreply.github.com>
2022-03-25 19:53:48 +0100

    Added ClearScript for upcoming clientside
    Example code for "test.js": ```script.SendMessage('test message');```

---
+++ Client/JavascriptHook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Microsoft.ClearScript.V8;
+
+using GTA;
+
+namespace CoopClient
+{
+    /// <summary>
+    /// Don't use this!
+    /// </summary>
+    public class JavascriptHook : Script
+    {
+        private bool LoadedEngine = false;
+
+        private static V8ScriptEngine ScriptEngine;
+
+        /// <summary>
+        /// Don't use this!
+        /// </summary>
+        public JavascriptHook()
+        {
+            Tick += Ontick;
+        }
+
+        private void Ontick(object sender, EventArgs e)
+        {
+            if (!Main.MainNetworking.IsOnServer())
+            {
+                return;
+            }
+
+            if (!LoadedEngine)
+            {
+                ScriptEngine = new V8ScriptEngine();
+                LoadedEngine = true;
+
+                ScriptEngine.AddHostObject("script", new ScriptContext());
+                ScriptEngine.Execute(System.IO.File.ReadAllText("scripts\\test.js"));
+            }
+        }
+    }
+
+    /// <summary>
+    /// FOR JAVASCRIPT ONLY!
+    /// </summary>
+    public class ScriptContext
+    {
+        /// <summary>
+        /// Don't use this!
+        /// </summary>
+        public void SendMessage(string message)
+        {
+            Main.MainChat.AddMessage("JAVASCRIPT", message);
+        }
+    }
+}
