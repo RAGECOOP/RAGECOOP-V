7a010a6da2f73f6822db89d56922ae537a27a360
EntenKoeniq <81123713+EntenKoeniq@users.noreply.github.com>
2022-04-10 14:44:56 +0200

    Update PlayerData.cs

--- Server/PlayerData.cs
+++ Server/PlayerData.cs
@@ -4,15 +4,15 @@ namespace CoopServer
 {
     public struct PlayerData
     {
-        public string Username { get; internal set; } = string.Empty;
-        private int _lastPedHandle { get; set; } = 0;
-        private int _currentPedHandle { get; set; } = 0;
+        public string Username { get; internal set; }
+        private int _lastPedHandle { get; set; }
+        private int _currentPedHandle { get; set; }
         public int PedHandle
         {
             get => _currentPedHandle;
             internal set
             {
-                _lastPedHandle = _currentPedHandle == 0 ? value : _currentPedHandle;
+                _lastPedHandle = _currentPedHandle == default ? value : _currentPedHandle;
                 _currentPedHandle = value;
 
                 if (_currentPedHandle != _lastPedHandle && Server.RunningResource != null)
@@ -21,14 +21,14 @@ namespace CoopServer
                 }
             }
         }
-        private int _lastVehicleHandle { get; set; } = 0;
-        private int _currentVehicleHandle { get; set; } = 0;
+        private int _lastVehicleHandle { get; set; }
+        private int _currentVehicleHandle { get; set; }
         public int VehicleHandle
         {
             get => _currentVehicleHandle;
             internal set
             {
-                _lastVehicleHandle = _currentVehicleHandle == 0 ? value : _currentVehicleHandle;
+                _lastVehicleHandle = _currentVehicleHandle == default ? value : _currentVehicleHandle;
                 _currentVehicleHandle = value;
 
                 if (_currentVehicleHandle != _lastVehicleHandle && Server.RunningResource != null)
@@ -37,9 +37,9 @@ namespace CoopServer
                 }
             }
         }
-        public bool IsInVehicle { get; internal set; } = false;
-        private LVector3 _lastPosition { get; set; } = new LVector3();
-        private LVector3 _currentPosition { get; set; } = new LVector3();
+        public bool IsInVehicle { get; internal set; }
+        private LVector3 _lastPosition { get; set; }
+        private LVector3 _currentPosition { get; set; }
         public LVector3 Position
         {
             get => _currentPosition;
@@ -54,14 +54,14 @@ namespace CoopServer
                 }
             }
         }
-        private int _lastHealth { get; set; } = 0;
-        private int _currentHealth { get; set; } = 0;
+        private int _lastHealth { get; set; }
+        private int _currentHealth { get; set; }
         public int Health
         {
             get => _currentHealth;
             internal set
             {
-                _lastHealth = _currentHealth == 0 ? value : _currentHealth;
+                _lastHealth = _currentHealth == default ? value : _currentHealth;
                 _currentHealth = value;
 
                 if (_currentHealth != _lastHealth && Server.RunningResource != null)
