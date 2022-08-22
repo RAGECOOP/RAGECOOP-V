39eb5ef80a1bced79577e0fbc70f1fb811673296
sardelka9515 <sardelka9515@gmail.com>
2022-08-22 19:38:04 +0800

    Hmm...

--- RageCoop.Server/Properties/AssemblyInfo.cs
+++ RageCoop.Server/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version information
-[assembly: AssemblyVersion("1.5.2.82")]
-[assembly: AssemblyFileVersion("1.5.2.82")]
+[assembly: AssemblyVersion("1.5.2.83")]
+[assembly: AssemblyFileVersion("1.5.2.83")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
