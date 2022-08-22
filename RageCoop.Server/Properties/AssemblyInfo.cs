a84ab42a4212b86d854ea78c5a4d78dd8f1c3585
sardelka9515 <sardelka9515@gmail.com>
2022-08-22 17:01:08 +0800

    blah

--- RageCoop.Server/Properties/AssemblyInfo.cs
+++ RageCoop.Server/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version information
-[assembly: AssemblyVersion("1.5.2.80")]
-[assembly: AssemblyFileVersion("1.5.2.80")]
+[assembly: AssemblyVersion("1.5.2.81")]
+[assembly: AssemblyFileVersion("1.5.2.81")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
